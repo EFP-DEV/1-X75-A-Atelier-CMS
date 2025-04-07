615bea5fb01d4c4211989b1f222c896a2ed906bb
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-04-07 21:19:55 +0200

    Add product catalog display and database connection

--- _partials/catalog.php
+++ _partials/catalog.php
@@ -1,5 +1,47 @@
 <div class="container px-4 px-lg-5 mt-5">
     <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+        <?php
+        require_once 'config/db.php';
+
+        $stmt = $pdo->query('SELECT * FROM products');
+        while ($product = $stmt->fetch()) {
+        ?>
+            <div class="col mb-5">
+                <div class="card h-100">
+                    <!-- Product image-->
+                    <img class="card-img-top" src="<?= $product['image']; ?>" alt="..." />
+                    <!-- Product details-->
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5 class="fw-bolder"><?php echo $product['name']; ?></h5>
+                            <?php
+                            if (!empty($product['rating'])) {
+                            ?>
+                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                    <?php
+                                    for($i = 0; $i< $product['rating']; ++$i){
+                                        echo '<div class="bi-star-fill"></div>';
+                                    }
+                                    ?>
+                             
+                                </div>
+                            <?php
+                            }
+                            ?>
+                            <!-- Product price-->
+                            $<?php echo $product['price_min']; ?> - $<?php echo $product['price_max']; ?>
+                        </div>
+                    </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                    </div>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
         <div class="col mb-5">
             <div class="card h-100">
                 <!-- Product image-->
